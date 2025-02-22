b0f45cf23c43ec03206a060775e5914eb74cc324
klefterov <klefterov@jacobs-university.de>
2025-02-22 15:20:31 +0100

    feat(router): implemented basic router endpoints for login,signup,verify

--- src/main.go
+++ src/main.go
@@ -2,8 +2,61 @@
 // infrastructure (route handling + database) for the Bricked-Up website.
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+const Port = ":3100"
+
+//endpoints map
+var endpoints = map[string]http.HandlerFunc {
+    "/login" : loginHandler,
+    "/signup" : signupHandler,
+    "/verify" : verifyHandler,
+}
+
+//loginHandler, signupHanlder, verifyHandler : helper funcs. for connecting the endpoints 
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method !=  "GET" {
+        http.Error(w, "Method unsupported", http.StatusMethodNotAllowed)
+        return
+    }
+    fmt.Fprintf(w, "TODO : login")
+}
+
+func signupHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        http.Error(w, "Method unsupported", http.StatusMethodNotAllowed)
+        return
+    }
+    fmt.Fprintf(w, "TODO : signup")
+}
+
+func verifyHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method != "GET" {
+        http.Error(w, "Method unsupported", http.StatusMethodNotAllowed)
+        return
+    }
+    fmt.Fprintf(w, "TODO : verify")
+}
+
+//mainHanlder uses the endpoints map to route requests.
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+    if handler, ok := endpoints[r.URL.Path] ; ok {
+        handler(w,r)
+        return
+    }
+    http.NotFound(w, r)
+}
 
 func main() {
-    fmt.Println("Hello World")
+    http.HandleFunc ("/", mainHandler)
+    
+    fmt.Printf("Router starting...")
+    if err := http.ListenAndServe(Port, nil); err != nil {
+        log.Fatal(err)
+    }
+
 }
