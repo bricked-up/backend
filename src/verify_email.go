0940e611e168239c727db82814aaa284033a7d11
akamran <a.kamran1128@gmail.com>
2025-03-03 00:12:40 +0100

    Commit12

--- src/verify_email.go
+++ src/verify_email.go
@@ -2,7 +2,6 @@ package backend
 
 import (
 	"database/sql"
-	"fmt"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -15,12 +14,12 @@ func verifyUser(verificationCode string) error {
 	// Remove expired verification codes
 	db, err := sql.Open("sqlite", "bricked-up_prod.db")
 	if err != nil {
-		return fmt.Errorf("failed to launch the database")
+		return err
 	}
 
 	_, err = db.Exec("DELETE FROM verification_codes WHERE expires_at < ?", time.Now())
 	if err != nil {
-		return fmt.Errorf("failed to remove expired verification codes: %v", err)
+		return err
 	}
 
 	// Verify the provided verification code
@@ -28,15 +27,15 @@ func verifyUser(verificationCode string) error {
 	err = db.QueryRow("SELECT user_id FROM verification_codes WHERE code = ? AND expires_at >= ?", verificationCode, time.Now()).Scan(&userID)
 	if err != nil {
 		if err == sql.ErrNoRows {
-			return fmt.Errorf("invalid verification code")
+			return err
 		}
-		return fmt.Errorf("failed to verify code: %v", err)
+		return err
 	}
 
 	// Set user's verifyid to NULL
 	_, err = db.Exec("UPDATE user SET verifyid = NULL WHERE id = ?", userID)
 	if err != nil {
-		return fmt.Errorf("failed to update user verification status: %v", err)
+		return err
 	}
 
 	return nil
