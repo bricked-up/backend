bfc152f8de61680dfda129a92321cc43c6027717
akamran <a.kamran1128@gmail.com>
2025-03-02 23:57:15 +0100

    Commit Create Issue

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.24
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	modernc.org/sqlite v1.36.0
+	
 )
 
 require (
--- src/validate_reset_token.go
+++ src/validate_reset_token.go
@@ -2,7 +2,6 @@ package backend
 
 import (
 	"database/sql"
-	"fmt"
 	"time"
 
 	_ "modernc.org/sqlite"
@@ -14,18 +13,18 @@ func RequestPasswordReset(token string, userEmail string) (string, error) {
 
 	db, err := sql.Open("sqlite", "bricked-up_prod.db")
 	if err != nil {
-		return "", fmt.Errorf("failed to launch the database")
+		return "", err
 	}
 
 	err = db.QueryRow("SELECT email, reset_token_expires FROM users WHERE reset_token = ?", token).Scan(&email, &expiry)
 	if err == sql.ErrNoRows {
-		return "", fmt.Errorf("expired or invalid token")
+		return "", err
 	} else if err != nil {
 		return "", err
 	}
 
 	if time.Now().After(expiry) {
-		return "", fmt.Errorf("expired token")
+		return "", err
 	}
 	db.Close()
 	return email, nil
