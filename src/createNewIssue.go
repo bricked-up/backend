f16a0afa16988e8d20b3fb41a68ff46702b7e512
akamran <a.kamran1128@gmail.com>
2025-03-06 16:25:08 +0100

    c1

--- src/createNewIssue.go
+++ src/createNewIssue.go
@@ -7,27 +7,25 @@ import (
 	_ "modernc.org/sqlite"
 )
 
-// CreateNewIssue creates a new issue in the database
-// inputs arethe title, description, tagid, priorityid, completed, cost, date, and ID
-// returns are the id and error
-func CreateNewIssue(title string, description string, tagid int, priorityid int, completed time.Time, cost int, date time.Time, ID int) (int64, error) {
-	//Open the database
+// date := time.Now()
+func CreateNewIssue(issueid int, title string, description string, tagid int, priorityid int, completed time.Time, cost int, date time.Time, ID int) (int64, error) {
+	//currentTimestamp := time.Now().UnixNano() / int64(time.Microsecond)
+	//uniqueID := uuid.New().ID()
+	//ID := currentTimestamp + int64(uniqueID)
+
 	db, err := sql.Open("sqlite", "bricked-up_prod.db")
 	if err != nil {
 		return -1, err
 	}
 	defer db.Close()
 
-	//Insert the new issue into the database
-	issue, err := db.Exec("INSERT INTO issues (title, tagid, priority, created, completed, cost, issueid) VALUES (?, ?, ?, ?, ?, ?, ?)", title, tagid, priorityid, date, completed, cost, ID)
+	issue, err := db.Exec("INSERT INTO issues (title, description, tagid, priority, created, completed, cost, issueid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", title, description, tagid, priorityid, date, completed, cost, ID)
 	if err != nil {
 		return -1, err
 	}
-	// Get the id of the new issue
 	id, err := issue.LastInsertId()
 	if err != nil {
 		return -1, err
 	}
-	//Return the id and nil
 	return id, nil
 }
