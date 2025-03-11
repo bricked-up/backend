9639b395ef68bac7db0294b1cec8441511db2368
akamran <a.kamran1128@gmail.com>
2025-03-11 15:26:54 +0100

    Added db as a paremeter

--- src/createNewIssue.go
+++ src/createNewIssue.go
@@ -10,18 +10,14 @@ import (
 // CreateNewIssue creates a new issue in the database with the given parameters.
 // Input: issueid, title, description, tagid, priorityid, completed, cost, date, ID
 // Output: int64, errors
-func CreateNewIssue(issueid int, title string, tagid int, priorityid int, completed time.Time, cost int, date time.Time, ID int) (int64, error) {
-
-	db, err := sql.Open("sqlite", "bricked-up_prod.db")
-	if err != nil {
-		return -1, err
-	}
-	defer db.Close()
+func CreateNewIssue(issueid int, title string, tagid int, priorityid int, completed time.Time, cost int, date time.Time, ID int, db *sql.DB) (int64, error) {
 
 	issue, err := db.Exec("INSERT INTO issues (title, tagid, priority, created, completed, cost, issueid) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", title, tagid, priorityid, date, completed, cost, ID)
 	if err != nil {
 		return -1, err
 	}
+
+	defer db.Close()
 	id, err := issue.LastInsertId()
 	if err != nil {
 		return -1, err
