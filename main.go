967b70ece9a60e7a825867328ec9648e8fe193a4
akamran <a.kamran1128@gmail.com>
2025-03-12 16:04:41 +0100

    Lol

--- main.go
+++ main.go
@@ -15,19 +15,9 @@ const PORT = ":3100" // Server port number.
 // Main sets up the server and starts listening on the defined PORT.
 // It registers MainHandler to process all incoming HTTP requests.
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-        db, err := sql.Open("sqlite3", os.Getenv("DB"))
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            log.Panic(err)
-        }
-
-        defer db.Close()
-        backend.MainHandler(db, w, r);
-    })
-
-		db, err := sql.Open("sqlite3", "bricked-up_prod.db")
+		db, err := sql.Open("sqlite3", os.Getenv("DB"))
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			log.Panic(err)
@@ -36,6 +26,15 @@ func main() {
 		defer db.Close()
 		backend.MainHandler(db, w, r)
 	})
+
+	db, err := sql.Open("sqlite3", "bricked-up_prod.db")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Panic(err)
+	}
+
+	defer db.Close()
+	backend.MainHandler(db, w, r)
 
 	log.Printf("Listening on localhost%s", PORT)
 	if err := http.ListenAndServe(PORT, nil); err != nil {
--- src/VerifyEmail.go
+++ src/VerifyEmail.go
@@ -11,27 +11,22 @@ import (
 // It removes all expired verification codes from the database and sets the user's
 // verifyid to NULL if the verification code is correct and has not expired.
 func VerifyUser(verificationCode string, db *sql.DB) error {
-	var err error
-	_, err = db.Exec("DELETE FROM verification_codes WHERE expires_at < ?", time.Now())
+	_, err := db.Exec("DELETE FROM verification_codes WHERE expires_at < ?", time.Now())
 	if err != nil {
 		return err
 	}
 
-	// Verify the provided verification code
 	var userID int
 	err = db.QueryRow("SELECT user_id FROM verification_codes WHERE code = ? AND expires_at >= ?", verificationCode, time.Now()).Scan(&userID)
 	if err != nil {
-		if err == sql.ErrNoRows {
-			return err
-		}
 		return err
 	}
 
-	// Set user's verifyid to NULL
 	_, err = db.Exec("UPDATE user SET verifyid = NULL WHERE id = ?", userID)
 	if err != nil {
 		return err
 	}
-	db.Close()
+
+	// Removed db.Close() here!
 	return nil
 }
