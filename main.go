ee47642ff6e3586af9307d7bae77dab44b7229ea
Rumen <rumenmitov@protonmail.com>
2025-03-27 12:46:46 +0100

    feat(logs, closes #125): added option for logs to be redirecte to a file

--- main.go
+++ main.go
@@ -14,6 +14,20 @@ const PORT = ":3100" // Server port number.
 // Main sets up the server and starts listening on the defined PORT.
 // It registers MainHandler to process all incoming HTTP requests.
 func main() {
+
+    // Persistent log file
+    logFilePath := os.Getenv("LOGS")
+    if (logFilePath != "") {
+        logFile, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+        if err != nil {
+            log.Panic(err)
+        }
+
+        defer logFile.Close();
+
+        log.SetOutput(logFile)
+    }
+
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
         db, err := sql.Open("sqlite", os.Getenv("DB"))
