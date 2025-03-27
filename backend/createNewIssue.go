4e1053732ab51920dce105e7249050eabae1fcb9
Emil <86645402+emilmoryakov@users.noreply.github.com>
2025-03-27 10:03:26 +0100

    refactor<Changed db to have priorities as an integer, closes #105> (#121)
    * Changed db to have priorities as an integer
    
    * Fixed issues
    
    * fix: broken references to priorityid
    
    ---------
    
    Co-authored-by: Rumen <rumenmitov@protonmail.com>

--- backend/createNewIssue.go
+++ backend/createNewIssue.go
@@ -9,13 +9,13 @@ import (
 )
 
 // CreatexNewIssue creates a new issue in the database with the given parameters.
-func CreateIssue(title string, desc string, tagid int, priorityid int, completed time.Time, cost int, date time.Time, db *sql.DB) (int64, error) {
+func CreateIssue(title string, desc string, tagid int, priority int, completed time.Time, cost int, date time.Time, db *sql.DB) (int64, error) {
 	title = utils.SanitizeText(title, utils.TEXT)
 	desc = utils.SanitizeText(desc, utils.TEXT)
 	issue, err := db.Exec(
-		`INSERT INTO issue (title, "desc", tagid, priorityid, created, completed, cost) 
+		`INSERT INTO issue (title, "desc", tagid, priority, created, completed, cost) 
 		 VALUES (?, ?, ?, ?, ?, ?, ?)`,
-		title, desc, tagid, priorityid, date, completed, cost,
+		title, desc, tagid, priority, date, completed, cost,
 	)
 	if err != nil {
 		return -1, err
