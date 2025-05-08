4a6d20714b7d7070997f747930501548792b0858
Rumen Mitov <108842741+rumenmitov@users.noreply.github.com>
2025-05-08 22:20:24 +0200

    hotfix: getuserhandler double serialization (#158)

--- backend/endpoints/users.go
+++ backend/endpoints/users.go
@@ -171,7 +171,7 @@ func GetUserHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(user))
+	w.Write(user)
 }
 
 // GetAllUsersHandler handles GET requests to retrieve all verified users. 
