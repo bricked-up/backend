3f0d5260ab4dd727fc74238dbdd55d470378fc72
Rumen <rumenmitov@protonmail.com>
2025-05-09 21:55:11 +0200

    hotfix: cookie can now be set over http

--- backend/endpoints/users.go
+++ backend/endpoints/users.go
@@ -37,7 +37,7 @@ func LoginHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 		Name: 		LoginCookie,
 		Value:    	session,
 		Expires:  	time.Now().Add(12 * 30 * 24 * time.Hour),
-		Secure:   	true,
+		Secure:   	false,
 		HttpOnly: 	true,
 	}
 
