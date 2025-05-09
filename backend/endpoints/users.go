5fe1ae2069f4b49abe096e6b4ff659353619a63b
Rumen <rumenmitov@protonmail.com>
2025-05-09 22:40:11 +0200

    hotfix: cookie lax mode

--- backend/endpoints/users.go
+++ backend/endpoints/users.go
@@ -39,7 +39,7 @@ func LoginHandler(db *sql.DB, w http.ResponseWriter, r *http.Request) {
 		Expires:  	time.Now().Add(12 * 30 * 24 * time.Hour),
 		Secure:   	true,
 		HttpOnly: 	true,
-		SameSite:   http.SameSiteNoneMode,
+		SameSite:   http.SameSiteLaxMode,
 	}
 
 	http.SetCookie(w, cookie)
