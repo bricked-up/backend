cef8e2cefe9319a3f4d8f53ef0f6a9ac2164e1b9
Rumen <rumenmitov@protonmail.com>
2025-05-10 09:13:09 +0200

    hotfix: cors headers not applied when there is no origin

--- main.go
+++ main.go
@@ -32,11 +32,11 @@ func main() {
 		origin := r.Header.Get("Origin")
 		if origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 		}
 
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		// Handle preflight requests
 		if r.Method == http.MethodOptions {
